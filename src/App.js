669abbd3ff532c325460bb31dfb4fedb102b5756
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-09-28 15:51:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [ text, setText ] = useState("");
+  const [ time, setTime ] = useState(5)
+  const [ gameOn, setGameOn ] = useState(false);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const wordsCount = (str) => {
+    return str.split(' ').filter((char)=> char !== '').length
+  }
+
+  const handleClick = () => {
+    setGameOn(true);
+  }
+
+  useEffect(()=> {
+    if (gameOn && time !== 0) {
+      setTimeout(()=> setTime(prevTime=> prevTime-1),1000 );
+    } else if ( time === 0) {
+      setGameOn(false)
+    }
+  }, [time, gameOn])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's check how fast do you type!</h1>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time remaining: {time}</h4>
+      <button onClick={handleClick}>Start</button>
+      <h1>words count: {wordsCount(text)} </h1>
     </div>
   );
 }
