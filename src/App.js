77dcee6c208c2a1eec71524feb4558bcf6bf9416
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-09-28 19:12:07 +0200

    add functionality to button

--- src/App.js
+++ src/App.js
@@ -2,37 +2,51 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [ text, setText ] = useState("");
-  const [ time, setTime ] = useState(5)
-  const [ gameOn, setGameOn ] = useState(false);
+  const [text, setText] = useState("");
+  const [time, setTime] = useState(5);
+  const [gameOn, setGameOn] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+  const [isDisabled, setDisabled] = useState(false);
 
   const handleChange = (e) => {
     setText(e.target.value);
   };
 
   const wordsCount = (str) => {
-    return str.split(' ').filter((char)=> char !== '').length
-  }
+    return str.split(" ").filter((char) => char !== "").length;
+  };
 
-  const handleClick = () => {
+  const startGame = () => {
     setGameOn(true);
-  }
+    setTime(5);
+    setText("");
+    setWordCount(0);
+    setDisabled(true);
+  };
 
-  useEffect(()=> {
+  const endGame = () => {
+    setGameOn(false);
+    setWordCount(wordsCount(text));
+  };
+
+  useEffect(() => {
     if (gameOn && time !== 0) {
-      setTimeout(()=> setTime(prevTime=> prevTime-1),1000 );
-    } else if ( time === 0) {
-      setGameOn(false)
+      setTimeout(() => setTime((prevTime) => prevTime - 1), 1000);
+    } else if (time === 0) {
+      endGame();
+      setDisabled(false);
     }
-  }, [time, gameOn])
+  }, [time, gameOn]);
 
   return (
     <div className="App">
       <h1>Let's check how fast do you type!</h1>
       <textarea onChange={handleChange} value={text} />
       <h4>Time remaining: {time}</h4>
-      <button onClick={handleClick}>Start</button>
-      <h1>words count: {wordsCount(text)} </h1>
+      <button onClick={startGame} disabled={isDisabled}>
+        Start
+      </button>
+      <h1>words count: {wordCount} </h1>
     </div>
   );
 }
