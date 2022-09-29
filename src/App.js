fbf918f4a9cc2e328caa21b861bc69c4029acbbd
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-09-29 20:35:06 +0200

    init func lettesCount and refreshscreen

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const REMAINING_TIME = 5;
+
   const [text, setText] = useState("");
-  const [time, setTime] = useState(5);
+  const [time, setTime] = useState(REMAINING_TIME);
   const [gameOn, setGameOn] = useState(false);
   const [wordCount, setWordCount] = useState(0);
+  const [letterCount, setLetterCount] = useState(0);
   const [isDisabled, setDisabled] = useState(false);
 
   const handleChange = (e) => {
@@ -16,18 +19,30 @@ function App() {
     return str.split(" ").filter((char) => char !== "").length;
   };
 
+  const lettersCount = (str) => {
+    return str.split("").filter((char) => char !== " ").length;
+  };
+
   const startGame = () => {
     setGameOn(true);
-    setTime(5);
+    setTime(REMAINING_TIME);
     setText("");
     setWordCount(0);
+    setLetterCount(0);
     setDisabled(true);
   };
 
   const endGame = () => {
     setGameOn(false);
     setWordCount(wordsCount(text));
+    setLetterCount(lettersCount(text));
   };
+
+  const refreshScreen = () => {
+    setText('');
+    setWordCount(0);
+    setLetterCount(0);
+  }
 
   useEffect(() => {
     if (gameOn && time !== 0) {
@@ -40,13 +55,17 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Let's check how fast do you type!</h1>
+      <h1>Let's check how fast you type!</h1>
       <textarea onChange={handleChange} value={text} disabled={!isDisabled} />
       <h4>Time remaining: {time}</h4>
-      <button onClick={startGame} disabled={isDisabled}>
-        Start
-      </button>
+      <div className="btn-container">
+        <button onClick={startGame} disabled={isDisabled}>
+          Start
+        </button>
+        <button onClick={refreshScreen} disabled={isDisabled}>Refresh</button>
+      </div>
       <h1>words count: {wordCount} </h1>
+      <h1>letters count: {letterCount} </h1>
     </div>
   );
 }
