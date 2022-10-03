454cf8d9c26fa6be7591655949513ff5732d6d10
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-10-03 19:33:57 +0200

    edit styles

--- src/App.js
+++ src/App.js
@@ -18,19 +18,25 @@ function App() {
   return (
     <div className="App">
       <h1>Let's check how fast you type!</h1>
-      <textarea onChange={handleChange} value={text} disabled={!isDisabled} ref={inputRef} />
+      <textarea
+        onChange={handleChange}
+        value={text}
+        disabled={!isDisabled}
+        ref={inputRef}
+      />
       <h2>Time remaining: {time}</h2>
       <div className="btn-container">
         <button onClick={startGame} disabled={isDisabled}>
           Start
         </button>
-        <button onClick={refreshScreen} disabled={isDisabled}>Refresh</button>
+        <button onClick={refreshScreen} disabled={isDisabled}>
+          Refresh
+        </button>
       </div>
       <div className="count-container">
-      <h1>words count: {wordCount} </h1>
-      <h1>letters count: {letterCount} </h1>
+        <h1>words count: {wordCount} </h1>
+        <h1>letters count: {letterCount} </h1>
       </div>
-      
     </div>
   );
 }
